d81e19c21a701812472503ff309db5d4a9a54674
burgersmoke <burgersmoke@gmail.com>
2020-12-13 22:30:50 -0700

    Updating about.py preparing to do a PyPy registration.

--- quickumls/about.py
+++ quickumls/about.py
@@ -3,10 +3,10 @@
 # https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
 # https://github.com/explosion/spaCy/blob/master/spacy/about.py
 
-__title__ = 'quickumls'
-__version__ = '1.4.0r1'
-__author__ = 'Luca Soldaini'
-__email__ = 'luca@ir.cs.georgetown.edu'
+__title__ = 'medspacy_quickumls'
+__version__ = '2.0'
+__author__ = 'Luca Soldaini, modified by Kelly Peterson'
+__email__ = 'luca@ir.cs.georgetown.edu,kelly.peterson@hsc.utah.edu'
 __license__ = 'MIT'
-__uri__ = "https://github.com/Georgetown-IR-Lab/QuickUMLS"
-__copyright__ = '2014-2020, Georgetown University Information Retrieval Lab'
+__uri__ = "https://github.com/medspacy/QuickUMLS"
+__copyright__ = '2014-2020, Georgetown University Information Retrieval Lab,2020, University of Utah'
