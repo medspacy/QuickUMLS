3aae4bc6ab2c93e5e388901a75f018a0c5231e40
Kelly Peterson <burgersmoke@gmail.com>
2022-09-07 13:53:49 -0600

    Adding a single test which currently passes which merely sets up a blank pipeline for testing.

---
+++ tests/test_quickumls_component.py
@@ -0,0 +1,37 @@
+import spacy
+import warnings
+from sys import platform
+import pytest
+
+from quickumls import spacy_component
+
+class TestQuickUMLSComponent:
+    @staticmethod
+    def can_test_quickumls():
+        if platform.startswith("win"):
+            # we're done here for now...
+            return False
+
+        return True
+
+    def test_simple_pipeline(self):
+        # let's make sure that this pipe has been initialized
+        # At least for MacOS and Linux which are currently supported...
+
+        if not TestQuickUMLSComponent.can_test_quickumls():
+            return
+
+        # allow default QuickUMLS (very small sample data) to be loaded
+        nlp = spacy.blank("en")
+
+        nlp.add_pipe("medspacy_quickumls")
+
+        assert nlp
+
+        quickumls = nlp.get_pipe("medspacy_quickumls")
+
+        assert quickumls
+        # this is a member of the QuickUMLS algorithm inside the component
+        assert quickumls.quickumls
+        # Check that the simstring database exists
+        assert quickumls.quickumls.ss_db
