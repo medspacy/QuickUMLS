59c0ec97cabe5288f3229823c3f9c9c9321eaf3b
Jianlin Shi <jianlins@users.noreply.github.com>
2024-03-04 13:47:30 -0700

    split pytest cases

--- tests/test_quickumls_component.py
+++ tests/test_quickumls_component.py
@@ -30,6 +30,8 @@ class TestQuickUMLSSpangroup(unittest.TestCase):
 
         quickumls = nlp.get_pipe("medspacy_quickumls")
 
+        print(quickumls.quickumls.info)
+
         assert quickumls
         # this is a member of the QuickUMLS algorithm inside the component
         assert quickumls.quickumls
@@ -38,41 +40,7 @@ class TestQuickUMLSSpangroup(unittest.TestCase):
 
 
 
-    def test_min_similarity_threshold(self):
-        """
-        Test that an extraction is NOT made if we set our matching to be perfect matching (100% similarity)
-        and we have a typo
-        """
 
-        # let's make sure that this pipe has been initialized
-        # At least for MacOS and Linux which are currently supported...
-
-        # allow default QuickUMLS (very small sample data) to be loaded
-        nlp = spacy.blank("en")
-
-        nlp.add_pipe("medspacy_quickumls", config={"threshold": 1.0, "quickumls_fp":self.quickumls_fp})
-
-        concept_term = "dipalmitoyllecithin"
-        # Let's turn this into a typo which will no longer match...
-        concept_term += 'n'
-
-        text = "Decreased {} content found in lung specimens".format(concept_term)
-
-        doc = nlp(text)
-        comp = nlp.get_pipe('medspacy_quickumls')
-        print(comp.result_type)
-
-        assert len(doc.ents) == 0
-
-
-        nlp = spacy.blank("en")
-
-        nlp.add_pipe("medspacy_quickumls", config={"threshold": .9,"quickumls_fp":self.quickumls_fp})
-
-        doc = nlp(text)
-
-
-        assert len(doc.ents) == 1
 
     def test_ensure_match_objects(self):
         """
@@ -87,6 +55,9 @@ class TestQuickUMLSSpangroup(unittest.TestCase):
         nlp = spacy.blank("en")
 
         nlp.add_pipe("medspacy_quickumls", config={"threshold": 1.0, "quickumls_fp":self.quickumls_fp})
+
+        pathexist=Path(self.quickumls_fp).exists()
+        print(self.quickumls_fp, pathexist)
 
         concept_term = "dipalmitoyllecithin"
 
