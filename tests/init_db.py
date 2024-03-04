f3c76fe877db364b15f6e887261e462861ee894a
Jianlin Shi <jianlins@users.noreply.github.com>
2024-03-03 21:48:10 -0700

    test

--- tests/init_db.py
+++ tests/init_db.py
@@ -12,6 +12,8 @@ C0000001|ENG|S|L0000002|VO|S0000895|N|A0000031||||BI|PT|BI00001|dipalmitoyl phos
 C0000002|ENG|S|L0000003|VO|S0000005|N|A0000041||||BI|PT|BI00002|glycosyltransferase|2|N|256|'''
     mrsty_text=r'''C0000001|T191|||||
 C0000002|T191|||||'''
+    if not Path(resource_folder).exists():
+        Path(resource_folder).mkdir(parents=True)
     Path(resource_folder,'MRCONSO.RRF').write_text(mrconso_text)
     Path(resource_folder,'MRSTY.RRF').write_text(mrsty_text)
     if Path(quickumls_fp).exists():        
