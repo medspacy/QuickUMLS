1eb00195d564f6a44a7d8875b35f2db72b319c61
Jianlin Shi <jianlins@users.noreply.github.com>
2024-03-03 21:37:53 -0700

    skip rebuild

--- tests/init_db.py
+++ tests/init_db.py
@@ -1,10 +1,12 @@
 from unittest.mock import patch
 from quickumls.install import main
 from pathlib import Path
-def init(resource_folder='output', quickumls_fp=str(Path('output', 'QuickUMLS_SAMPLE_lowercase_UNQLITE'))):
+def init(resource_folder='output', quickumls_fp=str(Path('output', 'QuickUMLS_SAMPLE_lowercase_UNQLITE')), force_rebuild=False):
     '''
     Make a demo UMLS dictionary for testing. The following items are using fake CUIs and TUIs.
     '''
+    if Path(quickumls_fp).exists() and not force_rebuild:
+        return   
     mrconso_text=r'''C0000001|ENG|S|L0000002|VO|S0000895|N|A0000030||||BI|PT|BI00001|dipalmitoyllecithin|2|N|256|
 C0000001|ENG|S|L0000002|VO|S0000895|N|A0000031||||BI|PT|BI00001|dipalmitoyl phosphatidylcholine|2|N|256|
 C0000002|ENG|S|L0000003|VO|S0000005|N|A0000041||||BI|PT|BI00002|glycosyltransferase|2|N|256|'''
